5301d83a3b6b365c1c993a60a928e7fe34fa693d
Damian <przpiw@gmail.com>
2021-12-27 20:50:50 +1030

    added google tag manager

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,10 @@
 import '../styles/globals.css'
+import TagManager from 'react-gtm-module'
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: process.env.GTM_ID })
+  }, [])
   return <Component {...pageProps} />
 }
 
