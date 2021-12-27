bcc14a2b6ed453fd2851a11210e21f463f9d9560
Damian <przpiw@gmail.com>
2021-12-27 23:34:29 +1030

    GTM fixes

--- pages/_document.js
+++ pages/_document.js
@@ -12,14 +12,14 @@ class MyDocument extends Document {
                 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','${NEXT_PUBLIC_GTM_ID}');`,
+                })(window,document,'script','dataLayer','NQRCMZP');`,
             }}
           />
           {/* End Google Tag Manager */}
           {/* Google Tag Manager (noscript) */}
           <noscript
             dangerouslySetInnerHTML={{
-              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${NEXT_PUBLIC_GTM_ID}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=NQRCMZP" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
             }}
           />
           {/* End Google Tag Manager (noscript) */}
